d9ce84299a5d1a51ea6686dd011bc502d9fbe630
showmewhatareyouworth <arsenijromanov26@gmail.com>
2025-02-03 17:48:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,189 @@
+// let a = 1;
+// let b = 3;
+// let c = 2;
+// alert(a*b/c);
+
+// let str = "abc";
+// console.log(str); 
+// let str2 = "asfe2qwdf";
+// console.log(str2);
+
+// let str = "abc" + "def";
+// console.log(str);
+
+// let str1 = "abc";
+// let str2 = "def";
+// console.log(str1 + str2);
+
+// let str1 = "abc";
+// let str2 = "def";
+// console.log(str1 + "!!!" + str2);
+
+// let str1 = "abc";
+// let str2 = "def";
+// console.log(str1 + " " + str2);
+
+// let str = "abc";
+// console.log(str + " " + "def");
+
+// let str = "abc";
+// console.log(str + " def");
+
+// let str = "abcde";
+// console.log(str.length);
+// let str2 = "abcde";
+// console.log("abcde".length);
+// let str3 = "abcde";
+// console.log("ab de".length);
+
+// let str = `abc`;
+// console.log(str);
+
+// let str = "xxx";
+// let txt = "aaa " + str + " bbb";
+// console.log(txt);
+
+// let str = "xxx";
+// let txt = `aaa ${str} bbb`;
+// console.log(txt);
+
+// console.log(myVar);
+// let myVar = 10;
+
+// if (true {   // SyntaxError: Unexpected token {
+//     console.log("Hello");
+// }
+
+// let num = 42;
+// num();
+// let obj = null;
+// console.log(obj.property);
+
+// let arr = new Array(-1);
+// function recursive() {
+// 	return recursive();
+// }
+
+// let a = "1234";
+// console.log("2" * "3" * "4");
+
+// console.log("2" * "3s");
+
+// let a = "1" + "2";
+// console.log(a);
+
+// let a = 1 + "2";
+// console.log(a);
+
+// let a = "1" + (2 + 3);
+// console.log(a);
+
+// let a = "1" + 2 + 3;
+// console.log(a);
+
+// let a = 2 + 3 + "1";
+// console.log(a);
+
+// let a = Number("2");
+// let b = Number("3");
+// console.log(a + b);
+
+// let a = +"2";
+// let b = +"3";
+// console.log(a + b);
+// let a = "2";
+// let b = +a;
+
+// let a = "2";
+// let b = "3";
+// console.log(+a + +b);
+
+// let num = parseInt("12px");
+// console.log(num);
+
+// let num = parseInt("12.5px");
+// console.log(num);
+
+// let num = parseFloat("12.5px");
+// console.log(num);
+
+// let num = parseFloat("12px");
+// console.log(num);
+
+// let str = String(123);
+// let num1 = 1;
+// let num2 = 2;
+
+// console.log(String(num1) + String(num2));
+
+// alert(String(true));
+// alert(String(false));
+
+// alert("a" + true);
+
+// alert(Number(true));
+// alert(Number(false));
+
+// alert(true + 1);
+// alert(true + true);
+
+// let str = "abcde";
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[2]);
+
+// let str = "abcde";
+// let num = 3;
+// console.log(str[num]);  
+
+// let str = "abcde";
+// str[0] = "!";
+
+// let str = "abcde";
+// let last = str.length - 1;
+// console.log(last);
+
+// let str = "abcde";
+// let last = str.length - 1;
+// console.log(str[last]);
+
+// let str = "abcde";
+// console.log(str[str.length - 1]);
+
+// let num = 1;
+// num += 3;
+// let num = 2;
+// num -= 3;
+// let num = 2;
+// num *= 3;
+// let num = 2;
+// num /= 3;
+
+// let num = 0;
+// num++;  
+// console.log(num);
+
+// let num = 0;
+// num--;  
+// console.log(num);
+
+// let num = 0;
+// console.log(num++);
+// console.log(num);
+
+// let num = 0;
+// console.log(++num);
+
+// let num1 = 0;
+// let num2 = num1++; 
+// console.log(num2);
+// console.log(num1);
+
+// let num1 = 0;
+// let num2 = ++num1;
+// console.log(num2);
+
+// let num = 0;
+// ++num;
+// num++;
+// console.log(num);
